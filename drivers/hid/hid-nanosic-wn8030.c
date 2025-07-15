9cb1be48e135432c4b0b3304257f8da67f9ac142
Fauzan Amir Al Ghiffary <alghiffaryfa19@gmail.com>
2025-07-15 08:35:00 +0000

    add log when kb stop

--- drivers/hid/hid-nanosic-wn8030.c
+++ drivers/hid/hid-nanosic-wn8030.c
@@ -502,7 +502,10 @@ static irqreturn_t nanosic_wn8030_handler(int irq, void *data)
 		break;
 	case 0x23:
 		nanosic_wn8030_handle_vendor(nanosic, buf);
+		print_hex_dump(KERN_INFO, "", DUMP_PREFIX_OFFSET, 16, 1, buf, sizeof(buf), false);
 		break;
+	default:
+		print_hex_dump(KERN_INFO, "", DUMP_PREFIX_OFFSET, 16, 1, buf, sizeof(buf), false);
 	}
 
 	//print_hex_dump(KERN_INFO, "", DUMP_PREFIX_OFFSET, 16, 1, buf, sizeof(buf), false);
